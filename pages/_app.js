b7b5508ef57bb8d9efbb7ca19a301f14367695c5
Vishal Maurya <mauryav518@gmail.com>
2022-10-26 20:45:08 +0530

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,18 @@ import Footer from "../components/Footer";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-
+import { Analytics } from "@vercel/analytics/react";
 
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-				<ThemeProvider attribute="class" enableSystem={true}>
-					<Navigation />
-					<Component {...pageProps} />
-					<Footer />
-				</ThemeProvider>
+			<ThemeProvider attribute="class" enableSystem={true}>
+				<Navigation />
+				<Component {...pageProps} />
+				<Analytics />
+				<Footer />
+			</ThemeProvider>
 		</>
 	);
 }
